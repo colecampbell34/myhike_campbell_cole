200475f298c685a6c684befcb70cd34228021bcb
Cole Campbell <ccampbell211@my.bcit.ca>
2024-10-29 10:52:36 -0700

    completed demo09 and added collections for users

--- scripts/main.js
+++ scripts/main.js
@@ -23,3 +23,17 @@ function getNameFromAuth() {
     });
 }
 getNameFromAuth(); //run the function
+
+// Function to read the quote of the day from the Firestore "quotes" collection
+// Input param is the String representing the day of the week, aka, the document name
+function readQuote(day) {
+    db.collection("quotes").doc(day)                                                         //name of the collection and documents should matach excatly with what you have in Firestore
+        .onSnapshot(dayDoc => {                                                              //arrow notation
+            console.log("current document data: " + dayDoc.data());                          //.data() returns data object
+            document.getElementById("quote-goes-here").innerHTML = dayDoc.data().quote;      //using javascript to display the data on the right place
+
+        }, (error) => {
+            console.log ("Error calling onSnapshot", error);
+        });
+    }
+    readQuote("tuesday");        //calling the function
